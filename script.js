d4e3faabf43de23eec998f8b6ca1619e8c74da4d
Sri Lakshmi K <srilakshmik1810@gmail.com>
2026-06-10 00:34:13 +0530

    portfolio website

---
+++ script.js
@@ -0,0 +1,34 @@
+function openModal(modalId) {
+    const modal = document.getElementById(modalId);
+
+    modal.style.display = "block";
+
+    setTimeout(() => {
+        modal.classList.add("show");
+    }, 10);
+}
+
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+
+    modal.classList.remove("show");
+
+    setTimeout(() => {
+        modal.style.display = "none";
+    }, 300);
+}
+
+window.onclick = function(event) {
+    const modals = document.querySelectorAll(".modal");
+
+    modals.forEach(modal => {
+        if (event.target === modal) {
+
+            modal.classList.remove("show");
+
+            setTimeout(() => {
+                modal.style.display = "none";
+            }, 300);
+        }
+    });
+};
